02ba15fc894fd30211be1e43f4805822dcc82276
hash22-glitch <hash22@mit.edu>
2026-09-23 14:00:52 -0400

    Add: longest-consecutive-sequence - submission-16

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-16.py
@@ -0,0 +1,33 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+
+        nums_set = set(nums)
+
+        our_chain = []
+
+        for num in nums:
+            # if len(our_chain)>1:
+            #     if  our_chain[0]<num<our_chain[-1]:
+            #         continue
+            if num-1 in nums_set:
+                continue
+            current_chain = [num]
+            i = 1
+            while True:
+                if num+i in nums_set:
+                    current_chain.append(num+i)
+                    i+=1
+
+                else:
+                    break
+            if len(current_chain) > len(our_chain):
+                our_chain = current_chain
+
+        return len(our_chain)
+            
+
+
+        
